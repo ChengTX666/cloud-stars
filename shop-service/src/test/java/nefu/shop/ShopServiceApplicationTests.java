78b808e2859c8f8548d5b1c0c7f1cb6ceeb17999
ChengTX666 <873661351@qq.com>
2024-11-21 16:19:06 +0800

    start

---
+++ shop-service/src/test/java/nefu/shop/ShopServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nefu.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
